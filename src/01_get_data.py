85651e70739c1a9fe983cfbc529de18e86fbe5cf
muthusm <69922862+muthusm@users.noreply.github.com>
2021-06-24 12:57:31 +0530

    edited get_data since dataset added in data directory

--- src/01_get_data.py
+++ src/01_get_data.py
@@ -1,5 +1,6 @@
 import os
 import kaggle
 
-kaggle.api.dataset_download_files('benroshan/factors-affecting-campus-placement', path='./data', force=False, unzip=True)
-os.rename('./data/Placement_Data_Full_Class.csv', './data/recruitment_details.csv')
+# kaggle.api.dataset_download_files('benroshan/factors-affecting-campus-placement', path='./data', force=False, unzip=True)
+# os.rename('./data/Placement_Data_Full_Class.csv', './data/recruitment_details.csv')
+print('Added dataset')
