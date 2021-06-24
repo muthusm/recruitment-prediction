ef7deb84d8571cda4b501555d5a54139e15fa3c0
muthusm <69922862+muthusm@users.noreply.github.com>
2021-06-24 13:00:30 +0530

    updated get_data.py

--- src/01_get_data.py
+++ src/01_get_data.py
@@ -1,5 +1,5 @@
 import os
-import kaggle
+# import kaggle
 
 # kaggle.api.dataset_download_files('benroshan/factors-affecting-campus-placement', path='./data', force=False, unzip=True)
 # os.rename('./data/Placement_Data_Full_Class.csv', './data/recruitment_details.csv')
